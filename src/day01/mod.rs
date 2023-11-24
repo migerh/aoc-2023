5e98b60549ec3e8b2bbaf87c0fd3f735c4d445f4
Michael Gerhäuser <michael.gerhaeuser@gmail.com>
2023-11-24 19:58:36 +0100

    Prepare for AoC 2023
    Bootstrap a skeleton project for cargo-aoc with a solution to last years
    first day to test the dev stack.

---
+++ src/day01/mod.rs
@@ -0,0 +1,81 @@
+use anyhow::{Context, Error, Result};
+use itertools::Itertools;
+use std::str::FromStr;
+
+use crate::utils::AocError::*;
+
+#[derive(Debug)]
+pub struct Thing {
+    items: Vec<u32>,
+}
+
+impl FromStr for Thing {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let items = s
+            .lines()
+            .map(|l| Ok(l.parse::<u32>()?))
+            .collect::<Result<Vec<_>>>()?;
+        Ok(Thing { items })
+    }
+}
+
+#[aoc_generator(day01)]
+pub fn input_generator(input: &str) -> Result<Vec<Thing>> {
+    input
+        .split("\n\n")
+        .filter(|s| !s.is_empty())
+        .map(Thing::from_str)
+        .collect::<Result<Vec<_>>>()
+        .context("Error while parsing input")
+}
+
+#[aoc(day01, part1)]
+pub fn solve_part1(input: &[Thing]) -> Result<u32> {
+    let max = input
+        .iter()
+        .map(|elf| elf.items.iter().sum::<u32>())
+        .max()
+        .ok_or(GenericError)
+        .context("Error while finding the elf with the most food")?;
+    Ok(max)
+}
+
+#[aoc(day01, part2)]
+pub fn solve_part2(input: &[Thing]) -> Result<u32> {
+    let top_three = input
+        .iter()
+        .map(|elf| elf.items.iter().sum::<u32>())
+        .sorted()
+        .rev()
+        .take(3)
+        .sum();
+
+    Ok(top_three)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn sample() -> &'static str {
+        ""
+    }
+
+    fn input() -> Result<Vec<Thing>> {
+        input_generator(sample())
+    }
+
+    #[test]
+    fn part1_sample() -> Result<()> {
+        let data = input()?;
+        Ok(assert_eq!(0, solve_part1(&data)?))
+    }
+
+    #[test]
+    fn part2_sample() -> Result<()> {
+        let data = input()?;
+        Ok(assert_eq!(0, solve_part2(&data)?))
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+extern crate aoc_runner;
+#[macro_use]
+extern crate aoc_runner_derive;
+#[macro_use]
+extern crate lazy_static;
+extern crate memoize;
+extern crate num;
+extern crate pathfinding;
+extern crate regex;
+extern crate serde_json;
+
+pub mod day01;
+pub mod utils;
+
+aoc_lib! { year = 2023 }
