e436743e6800c9692a9908330d751981f0f93cb3
Michael Gerhaeuser <michael.gerhaeuser@inovex.de>
2023-12-01 06:42:54 +0100

    Add day 1

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,58 +1,82 @@
-use anyhow::{Context, Error, Result};
-use itertools::Itertools;
-use std::str::FromStr;
+use anyhow::{Context, Result};
 
 use crate::utils::AocError::*;
 
-#[derive(Debug)]
-pub struct Thing {
-    items: Vec<u32>,
-}
-
-impl FromStr for Thing {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self> {
-        let items = s
-            .lines()
-            .map(|l| Ok(l.parse::<u32>()?))
-            .collect::<Result<Vec<_>>>()?;
-        Ok(Thing { items })
+fn normalise_digit(s: &str) -> &str {
+    match s {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        v => v,
     }
 }
 
+fn from_str(s: &str, patterns: &[&str]) -> Result<u32> {
+    let first_p = patterns
+        .iter()
+        .enumerate()
+        .filter_map(|(i, n)| s.find(n).map(|p| (i, p)))
+        .min_by(|a, b| a.1.cmp(&b.1))
+        .ok_or(GenericError)
+        .context("Could not determine min value, it seems there are no numbers")?;
+    let first = normalise_digit(patterns[first_p.0]);
+
+    let s_rev = s.chars().rev().collect::<String>();
+    let last_p = patterns
+        .iter()
+        .map(|n| n.chars().rev().collect::<String>())
+        .enumerate()
+        .filter_map(|(i, n)| s_rev.find(&n).map(|p| (i, p)))
+        .min_by(|a, b| a.1.cmp(&b.1))
+        .ok_or(GenericError)
+        .context("Could not determine min value, it seems there are no numbers")?;
+    let last = normalise_digit(patterns[last_p.0]);
+
+    let value = format!("{}{}", first, last).parse::<u32>()?;
+    Ok(value)
+}
+
 #[aoc_generator(day01)]
-pub fn input_generator(input: &str) -> Result<Vec<Thing>> {
-    input
-        .split("\n\n")
+pub fn input_generator(input: &str) -> Result<Vec<String>> {
+    Ok(input
+        .lines()
         .filter(|s| !s.is_empty())
-        .map(Thing::from_str)
-        .collect::<Result<Vec<_>>>()
-        .context("Error while parsing input")
+        .map(|l| l.to_owned())
+        .collect::<Vec<_>>())
 }
 
 #[aoc(day01, part1)]
-pub fn solve_part1(input: &[Thing]) -> Result<u32> {
+pub fn solve_part1(input: &[String]) -> Result<u32> {
+    let patterns = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     let max = input
         .iter()
-        .map(|elf| elf.items.iter().sum::<u32>())
-        .max()
-        .ok_or(GenericError)
-        .context("Error while finding the elf with the most food")?;
+        .map(|l| from_str(l, &patterns))
+        .collect::<Result<Vec<_>>>()?
+        .iter()
+        .sum::<u32>();
     Ok(max)
 }
 
 #[aoc(day01, part2)]
-pub fn solve_part2(input: &[Thing]) -> Result<u32> {
-    let top_three = input
+pub fn solve_part2(input: &[String]) -> Result<u32> {
+    let patterns = [
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine",
+    ];
+    let max = input
         .iter()
-        .map(|elf| elf.items.iter().sum::<u32>())
-        .sorted()
-        .rev()
-        .take(3)
-        .sum();
+        .map(|l| from_str(l, &patterns))
+        .collect::<Result<Vec<_>>>()?
+        .iter()
+        .sum::<u32>();
 
-    Ok(top_three)
+    Ok(max)
 }
 
 #[cfg(test)]
@@ -60,22 +84,28 @@ mod test {
     use super::*;
 
     fn sample() -> &'static str {
-        ""
+        "two13nine
+eight1wo3three
+abcone26threexyz
+xtwone37four
+4nineeightseven2
+zoneight234
+7pqrst9sixteen"
     }
 
-    fn input() -> Result<Vec<Thing>> {
+    fn input() -> Result<Vec<String>> {
         input_generator(sample())
     }
 
     #[test]
     fn part1_sample() -> Result<()> {
         let data = input()?;
-        Ok(assert_eq!(0, solve_part1(&data)?))
+        Ok(assert_eq!(234, solve_part1(&data)?))
     }
 
     #[test]
     fn part2_sample() -> Result<()> {
         let data = input()?;
-        Ok(assert_eq!(0, solve_part2(&data)?))
+        Ok(assert_eq!(281, solve_part2(&data)?))
     }
 }
