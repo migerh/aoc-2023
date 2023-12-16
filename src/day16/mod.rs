19e9878d2b823f5436a930ba17cfa17c056632b1
Michael Gerhaeuser <michael.gerhaeuser@inovex.de>
2023-12-16 08:05:36 +0100

    Refactor day 16, solves both

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -1,9 +1,10 @@
-use anyhow::{Context, Error, Result};
+use anyhow::{Context, Result};
 use itertools::Itertools;
-use std::{collections::HashSet, str::FromStr, cmp::max};
+use std::{cmp::max, collections::HashSet};
 
 use crate::utils::AocError::*;
 
+type Set<T> = HashSet<T>;
 pub type Coords = (isize, isize);
 
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone, Hash)]
@@ -14,7 +15,7 @@ pub enum Direction {
     Left,
 }
 
-#[derive(Debug, Clone, Hash)]
+#[derive(Debug, Clone)]
 pub struct Beam {
     pos: Coords,
     dir: Direction,
@@ -43,16 +44,6 @@ impl Beam {
 
 #[aoc_generator(day16)]
 pub fn input_generator(input: &str) -> Result<Vec<Vec<char>>> {
-    let input = ".|...\\....
-|.-.\\.....
-.....|-...
-........|.
-..........
-.........\\
-..../.\\\\..
-.-.-/..|..
-.|....-|.\\
-..//.|....";
     Ok(input
         .lines()
         .filter(|s| !s.is_empty())
@@ -70,39 +61,20 @@ pub fn is_within(map: &[Vec<char>], pos: &Coords) -> bool {
 pub fn trace(
     map: &[Vec<char>],
     beam: &Beam,
-    visited: &HashSet<(Coords, Direction)>,
-) -> HashSet<Coords> {
+    path: &mut Set<Coords>,
+    visited: &mut Set<(Coords, Direction)>,
+) {
     use Direction::*;
 
-    let debug = false;
-
-    let mut path = HashSet::new();
     if visited.contains(&(beam.pos, beam.dir.clone())) {
-        if debug {
-            println!();
-            println!("Already visited: {:?}", beam);
-            println!();
-        }
-        return path;
+        return;
     }
-    let mut passed = visited.clone();
-    passed.insert((beam.pos, beam.dir.clone()));
-
-    path.insert(beam.pos);
+    visited.insert((beam.pos, beam.dir.clone()));
 
     let mut current = beam.clone();
-    if debug {
-        println!();
-        println!("tracing {:?}", beam);
-    }
     loop {
         let tile = map[current.pos.1 as usize][current.pos.0 as usize];
-        if debug {
-            println!("{:?}, {}", current.pos, tile);
-            println!("size {}", path.len());
-        }
 
-        // -|/\
         match (tile, current.dir.clone()) {
             ('.', _) => {
                 path.insert(current.pos);
@@ -111,37 +83,23 @@ pub fn trace(
                 path.insert(current.pos);
             }
             ('-', d) if d == Up || d == Down => {
-                if debug {
-                    println!();
-                    println!("split -");
-                }
                 let left_beam = Beam::new(current.pos, Left);
-                let left = trace(map, &left_beam, &passed);
+                trace(map, &left_beam, path, visited);
                 let right_beam = Beam::new(current.pos, Right);
-                let right = trace(map, &right_beam, &passed);
+                trace(map, &right_beam, path, visited);
 
-                path.extend(left);
-                path.extend(right);
-
-                return path;
+                return;
             }
             ('|', d) if d == Up || d == Down => {
                 path.insert(current.pos);
             }
             ('|', d) if d == Left || d == Right => {
-                if debug {
-                    println!();
-                    println!("split |");
-                }
                 let up_beam = Beam::new(current.pos, Up);
-                let up = trace(map, &up_beam, &passed);
+                trace(map, &up_beam, path, visited);
                 let down_beam = Beam::new(current.pos, Down);
-                let down = trace(map, &down_beam, &passed);
+                trace(map, &down_beam, path, visited);
 
-                path.extend(up);
-                path.extend(down);
-
-                return path;
+                return;
             }
             ('\\', d) => {
                 path.insert(current.pos);
@@ -172,62 +130,72 @@ pub fn trace(
 
         current = current.next();
         if !is_within(map, &current.pos) {
-            return path;
+            return;
         }
 
-        if passed.contains(&(current.pos, current.dir.clone())) {
-            return path;
+        if visited.contains(&(current.pos, current.dir.clone())) {
+            return;
         }
-        passed.insert((current.pos, current.dir.clone()));
+        visited.insert((current.pos, current.dir.clone()));
     }
 }
 
 #[aoc(day16, part1)]
 pub fn solve_part1(input: &[Vec<char>]) -> Result<usize> {
-    input.iter().for_each(|l| {
-        println!("{}", l.iter().join(""));
-    });
-
     let start = Beam::new((0, 0), Direction::Right);
-    let visited = trace(input, &start, &HashSet::new());
+    let mut path = Set::new();
+    let mut visited = Set::new();
+    trace(input, &start, &mut path, &mut visited);
 
-    let width = input[0].len();
-    let height = input.len();
-    let mut visual = vec![vec!['.'; width]; height];
-    visited.iter().for_each(|p| {
-        visual[p.1 as usize][p.0 as usize] = '#';
-    });
+    // Ok(visited.iter().map(|(c, _)| c).collect::<Set<_>>().len())
 
-    visual.iter().for_each(|l| {
-        println!("{}", l.iter().join(""));
-    });
-
-    Ok(visited.len())
+    Ok(path.len())
 }
 
 #[aoc(day16, part2)]
 pub fn solve_part2(input: &[Vec<char>]) -> Result<usize> {
     let width = input[0].len() as isize;
     let height = input.len() as isize;
-    let passed = HashSet::new();
 
-    let max_x = (0..width).map(|x| {
-        let start_top = Beam::new((x, 0), Direction::Down);
-        let top = trace(input, &start_top, &passed).len();
-        let start_bottom = Beam::new((x, height - 1), Direction::Up);
-        let bottom = trace(input, &start_bottom, &passed).len();
+    let max_x = (0..width)
+        .map(|x| {
+            let start_top = Beam::new((x, 0), Direction::Down);
+            let mut path_top = Set::new();
+            let mut visited_top = Set::new();
+            trace(input, &start_top, &mut path_top, &mut visited_top);
+            let top = path_top.len();
 
-        max(top, bottom)
-    }).max().ok_or(GenericError).context("Could not find max item in x dir")?;
+            let start_bottom = Beam::new((x, height - 1), Direction::Up);
+            let mut path_bottom = Set::new();
+            let mut visited_bottom = Set::new();
+            trace(input, &start_bottom, &mut path_bottom, &mut visited_bottom);
+            let bottom = path_bottom.len();
 
-    let max_y = (1..height-1).map(|y| {
-        let start_left = Beam::new((0, y), Direction::Right);
-        let left = trace(input, &start_left, &passed).len();
-        let start_right = Beam::new((width - 1, y), Direction::Left);
-        let right = trace(input, &start_right, &passed).len();
+            max(top, bottom)
+        })
+        .max()
+        .ok_or(GenericError)
+        .context("Could not find max item in x dir")?;
 
-        max(left, right)
-    }).max().ok_or(GenericError).context("Could not find max item in y dir")?;
+    let max_y = (1..height - 1)
+        .map(|y| {
+            let start_left = Beam::new((0, y), Direction::Right);
+            let mut path_left = Set::new();
+            let mut visited_left = Set::new();
+            trace(input, &start_left, &mut path_left, &mut visited_left);
+            let left = path_left.len();
+
+            let start_right = Beam::new((width - 1, y), Direction::Left);
+            let mut path_right = Set::new();
+            let mut visited_right = Set::new();
+            trace(input, &start_right, &mut path_right, &mut visited_right);
+            let right = path_right.len();
+
+            max(left, right)
+        })
+        .max()
+        .ok_or(GenericError)
+        .context("Could not find max item in y dir")?;
 
     Ok(max(max_x, max_y))
 }
@@ -237,22 +205,31 @@ mod test {
     use super::*;
 
     fn sample() -> &'static str {
-        ""
+        ".|...\\....
+|.-.\\.....
+.....|-...
+........|.
+..........
+.........\\
+..../.\\\\..
+.-.-/..|..
+.|....-|.\\
+..//.|...."
     }
 
-    fn input() -> Result<Vec<Thing>> {
+    fn input() -> Result<Vec<Vec<char>>> {
         input_generator(sample())
     }
 
     #[test]
     fn part1_sample() -> Result<()> {
         let data = input()?;
-        Ok(assert_eq!(0, solve_part1(&data)?))
+        Ok(assert_eq!(46, solve_part1(&data)?))
     }
 
     #[test]
     fn part2_sample() -> Result<()> {
         let data = input()?;
-        Ok(assert_eq!(0, solve_part2(&data)?))
+        Ok(assert_eq!(51, solve_part2(&data)?))
     }
 }
