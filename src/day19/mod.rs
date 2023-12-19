7a9766f09b2ccc889f862f6056e5d21e513834e2
Michael Gerhaeuser <michael.gerhaeuser@inovex.de>
2023-12-19 07:13:45 +0100

    Day 19, Part 1

--- src/day19/mod.rs
+++ src/day19/mod.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Error, Result};
 use itertools::Itertools;
-use std::str::FromStr;
+use std::{str::FromStr, collections::HashMap};
 
 use crate::utils::AocError::*;
 
@@ -42,7 +42,7 @@ impl FromStr for Part {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Next {
     Accept,
     Reject,
@@ -61,7 +61,7 @@ impl FromStr for Next {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Property {
     X,
     M,
@@ -83,7 +83,7 @@ impl FromStr for Property {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Rule {
     Less(Property, u64, Next),
     Greater(Property, u64, Next),
@@ -140,7 +140,7 @@ impl FromStr for Rule {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Workflow {
     id: String,
     rules: Vec<Rule>,
@@ -173,23 +173,23 @@ impl FromStr for Workflow {
 
 #[aoc_generator(day19)]
 pub fn input_generator(input: &str) -> Result<(Vec<Workflow>, Vec<Part>)> {
-    let input = "px{a<2006:qkq,m>2090:A,rfg}
-pv{a>1716:R,A}
-lnx{m>1548:A,A}
-rfg{s<537:gd,x>2440:R,A}
-qs{s>3448:A,lnx}
-qkq{x<1416:A,crn}
-crn{x>2662:A,R}
-in{s<1351:px,qqz}
-qqz{s>2770:qs,m<1801:hdj,R}
-gd{a>3333:R,R}
-hdj{m>838:A,pv}
-
-{x=787,m=2655,a=1222,s=2876}
-{x=1679,m=44,a=2067,s=496}
-{x=2036,m=264,a=79,s=2244}
-{x=2461,m=1339,a=466,s=291}
-{x=2127,m=1623,a=2188,s=1013}";
+//     let input = "px{a<2006:qkq,m>2090:A,rfg}
+// pv{a>1716:R,A}
+// lnx{m>1548:A,A}
+// rfg{s<537:gd,x>2440:R,A}
+// qs{s>3448:A,lnx}
+// qkq{x<1416:A,crn}
+// crn{x>2662:A,R}
+// in{s<1351:px,qqz}
+// qqz{s>2770:qs,m<1801:hdj,R}
+// gd{a>3333:R,R}
+// hdj{m>838:A,pv}
+// 
+// {x=787,m=2655,a=1222,s=2876}
+// {x=1679,m=44,a=2067,s=496}
+// {x=2036,m=264,a=79,s=2244}
+// {x=2461,m=1339,a=466,s=291}
+// {x=2127,m=1623,a=2188,s=1013}";
     let mut split = input
         .split("\n\n");
 
@@ -199,10 +199,88 @@ hdj{m>838:A,pv}
     Ok((workflows, parts))
 }
 
+impl Rule {
+    pub fn next(&self, part: &Part) -> Option<&Next> {
+        println!("Checking {:?} against {:?}", part, self);
+        match self {
+            Rule::Greater(prop, num, next) => {
+                match prop {
+                    Property::X if part.x > *num => Some(next),
+                    Property::M if part.m > *num => Some(next),
+                    Property::A if part.a > *num => Some(next),
+                    Property::S if part.s > *num => Some(next),
+                    _ => None,
+                }
+            },
+            Rule::Less(prop, num, next) => {
+                match prop {
+                    Property::X if part.x < *num => Some(next),
+                    Property::M if part.m < *num => Some(next),
+                    Property::A if part.a < *num => Some(next),
+                    Property::S if part.s < *num => Some(next),
+                    _ => None,
+                }
+            }
+            Rule::Else(n) => Some(n),
+        }
+    }
+}
+
+impl Part {
+    pub fn rating(&self) -> u64 {
+        self.x + self.m + self.a + self.s
+    }
+
+    pub fn is_accepted(&self, workflows: &HashMap<String, Workflow>) -> bool {
+        println!("checking {:?}", self);
+        if let Some(in_workflow) = workflows.get("in") {
+            let mut workflow = in_workflow.clone();
+
+            loop {
+                println!();
+                println!("workflow: {}", workflow.id);
+                for rule in workflow.rules.clone() {
+                    println!("Rule {:?}", rule);
+                    if let Some(next) = rule.next(self) {
+                        match next {
+                            Next::Accept => return true,
+                            Next::Reject => {
+                                println!("Straight up reject");
+                                return false
+                            },
+                            Next::Workflow(wf) => {
+                                println!("Next workflow {:?}", wf);
+                                if let Some(w) = workflows.get(wf) {
+                                    workflow = w.clone();
+                                    break;
+                                } else {
+                                    println!("Workflow {} not found", wf);
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        println!("Ran out of rules?");
+        false
+    }
+}
+
 #[aoc(day19, part1)]
-pub fn solve_part1(input: &(Vec<Workflow>, Vec<Part>)) -> Result<u32> {
-    println!("{:?}", input);
-    Ok(0)
+pub fn solve_part1(input: &(Vec<Workflow>, Vec<Part>)) -> Result<u64> {
+    let (workflows, parts) = input;
+    let workflows = workflows.iter().cloned().map(|w| (w.id.clone(), w)).collect::<HashMap<String, Workflow>>();
+
+    let rating = parts.iter().filter_map(|p| if p.is_accepted(&workflows) {
+        Some(p.rating())
+    } else {
+        None
+    }).sum();
+
+    Ok(rating)
 }
 
 #[aoc(day19, part2)]
