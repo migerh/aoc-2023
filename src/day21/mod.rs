ab897a76f9156c2398fe659ded0edf8552b2a12d
Michael Gerhaeuser <michael.gerhaeuser@inovex.de>
2023-12-21 07:04:59 +0100

    Day 21, Part 2 works for a small number of steps

--- src/day21/mod.rs
+++ src/day21/mod.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Error, Result};
-use itertools::Itertools;
+use itertools::{Itertools, MinMaxResult};
 use std::{
     collections::{HashMap, HashSet, VecDeque},
     str::FromStr,
@@ -53,26 +53,41 @@ pub fn input_generator(input: &str) -> Result<Map> {
         .collect::<Result<HashMap<Coords, Tile>>>()
 }
 
-fn successors(map: &Map, pos: &Coords) -> Vec<Coords> {
-    [
-        (pos.0, pos.1 - 1),
-        (pos.0 + 1, pos.1),
-        (pos.0, pos.1 + 1),
-        (pos.0 - 1, pos.1),
-    ]
-    .into_iter()
-    .filter(|c| map.contains_key(c))
-    .filter_map(|c| Some((c, map.get(&c)?)))
-    .filter(|(c, t)| **t == Tile::Plot || **t == Tile::Start)
-    .map(|(c, _)| c)
-    .collect_vec()
+fn successors(map: &Map, pos: &Coords, size: &Option<Coords>) -> Vec<Coords> {
+    if let Some(s) = size {
+        [
+            (pos.0, pos.1 - 1),
+            (pos.0 + 1, pos.1),
+            (pos.0, pos.1 + 1),
+            (pos.0 - 1, pos.1),
+        ]
+        .into_iter()
+        .map(|c| (c, (c.0.rem_euclid(s.0), c.1.rem_euclid(s.1))))
+        .filter_map(|(p, c)| Some((p, c, map.get(&c)?)))
+        .filter(|(_, _, t)| **t == Tile::Plot || **t == Tile::Start)
+        .map(|(p, _, _)| p)
+        .collect_vec()
+    } else {
+        [
+            (pos.0, pos.1 - 1),
+            (pos.0 + 1, pos.1),
+            (pos.0, pos.1 + 1),
+            (pos.0 - 1, pos.1),
+        ]
+        .into_iter()
+        .filter(|c| map.contains_key(c))
+        .filter_map(|c| Some((c, map.get(&c)?)))
+        .filter(|(c, t)| **t == Tile::Plot || **t == Tile::Start)
+        .map(|(c, _)| c)
+        .collect_vec()
+    }
 }
 
-fn can_reach(map: &Map, pos: &Vec<Coords>) -> Vec<Coords> {
+fn can_reach(map: &Map, pos: &Vec<Coords>, size: &Option<Coords>) -> Vec<Coords> {
     let mut result = HashSet::new();
 
     for p in pos {
-        for n in successors(map, p) {
+        for n in successors(map, p, size) {
             result.insert(n);
         }
     }
@@ -91,15 +106,46 @@ pub fn solve_part1(input: &Map) -> Result<usize> {
 
     let mut result = vec![start];
     for i in 0..goal {
-        result = can_reach(input, &result);
+        result = can_reach(input, &result, &None);
     }
 
     Ok(result.len())
 }
 
+fn size(map: &Map) -> Result<Coords> {
+    let (_, max_x) = match map.iter().map(|(k, _)| k.0).minmax() {
+        MinMaxResult::NoElements => Err(GenericError).context("Map has no size")?,
+        MinMaxResult::OneElement(e) => (e, e),
+        MinMaxResult::MinMax(min, max) => (min, max),
+    };
+
+    let (_, max_y) = match map.iter().map(|(k, _)| k.1).minmax() {
+        MinMaxResult::NoElements => Err(GenericError).context("Map has no size")?,
+        MinMaxResult::OneElement(e) => (e, e),
+        MinMaxResult::MinMax(min, max) => (min, max),
+    };
+
+    Ok((max_x + 1, max_y + 1))
+}
+
 #[aoc(day21, part2)]
-pub fn solve_part2(input: &Map) -> Result<u32> {
-    Ok(0)
+pub fn solve_part2(input: &Map) -> Result<usize> {
+    let goal = 500; //26501365;
+    let start = input
+        .iter()
+        .find(|(_, v)| **v == Tile::Start)
+        .map(|(k, _)| *k)
+        .context("Could not find start")?;
+
+    let size = Some(size(input)?);
+
+    let mut result = vec![start];
+    for i in 0..goal {
+        result = can_reach(input, &result, &size);
+        println!("{}: {}", i, result.len());
+    }
+
+    Ok(result.len())
 }
 
 #[cfg(test)]
